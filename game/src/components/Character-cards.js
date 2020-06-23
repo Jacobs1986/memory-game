693657e81aa6cf602b4670a937585f4709cf9b8b
Sean Jacobson <sean.jacobson86@gmail.com>
2020-06-23 12:36:38 -0600

    Update Character-cards.js
    Changed the height of the images so that they wouldn't look so squashed.

--- game/src/components/Character-cards.js
+++ game/src/components/Character-cards.js
@@ -4,7 +4,7 @@ import Card from 'react-bootstrap/Card';
 function Character(props) {
     return (
         <Card style={{ width: '18rem' }}>
-            <Card.Img variant='top' src={props.image} style={{ height: '11rem' }} />
+            <Card.Img variant='top' src={props.image} style={{ height: '13rem' }} />
             <Card.Body>
                 <Card.Title>{props.name}</Card.Title>
             </Card.Body>
