c4eb9c1a7ad7fae338c2929185f6ec88dac36aed
Sean Jacobson <sean.jacobson86@gmail.com>
2020-06-23 12:00:09 -0600

    Update App.js
    Outline completed.

--- game/src/App.js
+++ game/src/App.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-
+import Col from 'react-bootstrap/Col';
 
 class App extends Component {
 
@@ -13,6 +13,20 @@ class App extends Component {
       <Container>
         <Row>
           <h1>Match Game!</h1>
+        </Row>
+        <Row>
+          Click each of the characters below to raise your score. Make sure you click each character only once! Can you beat your highscore?
+        </Row>
+        <Row>
+          <Col md={3}>
+            Score:
+          </Col>
+          <Col md={3}>
+            Highscore:
+          </Col>
+        </Row>
+        <Row>
+          Character cards go here
         </Row>
       </Container>
     );
