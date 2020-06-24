cd95f55ced5f04665d53d2e5859ca164aed38e77
Sean Jacobson <sean.jacobson86@gmail.com>
2020-06-23 21:02:52 -0600

    Update App.js
    I got a function that will shuffle the array.

--- game/src/App.js
+++ game/src/App.js
@@ -19,6 +19,29 @@ class App extends Component {
     score: 0
   }
 
+  shuffle = array => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+componentDidMount() {
+  this.setState({ characters: this.shuffle(characters)});
+}
+
   // Function to do something when the character is clicked.
   characterClick(event) {
     console.log(event.target.id);
